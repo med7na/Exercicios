ca6990d178e948a35f80c7178d0d1413be258dd0
Kelvin Medina <medinadev44@gmail.com>
2025-10-21 07:25:52 -0300

    Exercicio matriz

--- Exercicios/src/matriz.java
+++ Exercicios/src/matriz.java
@@ -8,16 +8,21 @@ public class matriz {
         int n = input.nextInt();
 
         int [][] mat = new int[m][n];
-        for(int i=0; i>mat.length; i++) {
-            for(int j=0; j>mat[i].length; j++) {
+        for(int i=0; i<mat.length; i++) {
+            for(int j=0; j<mat[i].length; j++) {
                 mat[i][j] = input.nextInt();
             }
         }
         int num = input.nextInt();
-        for(int i=0; i>mat.length; i++) {
-            for(int j=0; j>mat[i].length; j++) {
-                if(num == ) {
+        for(int i=0; i <mat.length; i++) {
+            for(int j= 0; j <mat[i].length; j++) {
+                if(mat[i][j] == num) {
+                    System.out.println("Position: " + i + "," + j);
 
+                    if(j > 0) System.out.println("Left: " + mat[i][j - 1]);
+                    if(j < n - 1) System.out.println("Right: " + mat[i][j + 1]);
+                    if(i > 0) System.out.println("Up: " + mat[i-1][j]);
+                    if(i < m - 1) System.out.println("Down: " + mat[i+1][j]);
                 }
     }
 }
